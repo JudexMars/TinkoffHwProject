92bec52d2291b4992957b1915b95b9c86be6ba7b
JudexMars <danielpro2013rus@gmail.com>
2024-02-17 11:08:17 +0300

    init: basic project initialization

---
+++ src/main/java/org/judexmars/tinkoffhwproject/TinkoffHwProjectApplication.java
@@ -0,0 +1,13 @@
+package org.judexmars.tinkoffhwproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffHwProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffHwProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/judexmars/tinkoffhwproject/TinkoffHwProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.judexmars.tinkoffhwproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffHwProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
